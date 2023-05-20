8997bb3d49f945aeaa054671e30c3e269dfa8d15
tarek1o <tarekeslam33456@gmail.com>
2023-05-20 09:08:32 +0300

    Create DTO_Classes folder

---
+++ MindMission.Infrastructure/DTO_Classes/DiscussionDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindMission.Infrastructure.DTO_Classes
+{
+    public class DiscussionDTO
+    {
+    }
+}
