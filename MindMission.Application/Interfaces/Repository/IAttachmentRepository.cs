f7ec7360e65f7f73ea25a1ea375211941616e8f5
Abdelrahman Elsayed <abd-elrahman_elsayed@outlook.com>
2023-05-25 01:52:24 +0300

    Post file as in bytes in database

---
+++ MindMission.Application/Interfaces/Repository/IAttachmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using MindMission.Domain.Enums;
+using MindMission.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindMission.Application.Interfaces.Repository
+{
+    public interface IAttachmentRepository
+    {
+        Task PostAttachmentAsync(Attachment Attachment);
+    }
+}
---
+++ MindMission/Controllers/AttachmentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MindMission.Application.DTOs;
+using MindMission.Application.Interfaces.Services;
+using MindMission.Application.Repository_Interfaces;
+
+namespace MindMission.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttachmentController : ControllerBase
+    {
+        private readonly IAttachmentService _attachmentService;
+
+        public AttachmentController(IAttachmentService attachmentService)
+        {
+            _attachmentService = attachmentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAttachment([FromForm] AttachmentDto attachmentDto)
+        {
+            if (attachmentDto == null)
+            {
+                return BadRequest("Entered Attachment is required");
+            }
+
+            if (ModelState.IsValid)
+            {
+                bool IsExistedLesson = await _attachmentService.IsExistedLesson(attachmentDto.LessonId);
+                if(IsExistedLesson)
+                    return Ok(await _attachmentService.AddAttachmentAsync(attachmentDto));
+                return BadRequest("NotExisted Lesson");
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
